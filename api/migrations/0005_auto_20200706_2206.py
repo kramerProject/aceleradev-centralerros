86476e0f692f616ba86a9c1c6b8a40fde0dba96c
anachecoli <anagchecoli@gmail.com>
2020-07-15 14:47:13 -0300

    Initial commit

--- api/migrations/0005_auto_20200706_2206.py
+++ api/migrations/0005_auto_20200706_2206.py
@@ -37,7 +37,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='agent',
             name='version',
-            field=models.CharField(default=django.utils.timezone.now, max_length=20),
+            field=models.CharField(default=django.utils.timezone.now, max_length=4),
             preserve_default=False,
         ),
     ]
